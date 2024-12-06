9f7826a2a2b57f664c81248317c37ca9c4fe8e52
vinend <vinendsocials@gmail.com>
2024-12-06 14:08:29 +0700

    Video done.

--- obj/Debug/net8.0/app.AssemblyInfo.cs
+++ obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ffc349ff79e2873258112ee01d6cc47aa1f628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4090e1ec5f277160934dcadeca40245a1249c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
